a9fc2a3c53bfc6cf4e70ff31038754b2cf32a2c1
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2024-03-06 03:44:11 +0530

    Update post.js

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,10 +3,10 @@ import { handleError } from "../error.js";
 import User from "../models/User.js";
 
 export const createPost = async (req, res, next) => {
-  const newpost = new post(req.body);
+  const newPost = new Post(req.body);
   try {
-    const savedpost = await newpost.save();
-    res.status(200).json(savedpost);
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
   } catch (err) {
     handleError(500, err);
   }
