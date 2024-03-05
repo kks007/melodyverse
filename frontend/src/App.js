b6cf382a6c2f14789c616e36e9306e3f90b07f0f
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2024-03-06 03:00:07 +0530

    set skeleton

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {//make a centered div saying "melodyverse"}
+      <div className="centered">
+        <h1>Melodyverse</h1>
+      </div>
+    }
+
+    </div>
+  );
+}
+
+export default App;
